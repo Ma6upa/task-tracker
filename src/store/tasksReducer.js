8da2584f4dd14752f88ae6c4d166086d36520d2c
Ma6upa <vaud55@gmail.com>
2023-04-25 16:37:45 +0400

    add tasks logic

--- src/store/tasksReducer.js
+++ src/store/tasksReducer.js
@@ -8,10 +8,12 @@ const initialState = {
 export const tasksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TASK:
-      return {...state, tasks: [...state.tasks, action.payload]}
-      case REMOVE_TASK:
-      return {...state, tasks: state.tasks.filter(task => task.id !== action.payload)}
-    default: 
+      localStorage.setItem('tasks', JSON.stringify([...state.tasks, action.payload]))
+      return { ...state, tasks: [...state.tasks, action.payload] }
+    case REMOVE_TASK:
+      localStorage.setItem('tasks', JSON.stringify(state.tasks.filter(task => task.id !== action.payload)))
+      return { ...state, tasks: state.tasks.filter(task => task.id !== action.payload) }
+    default:
       return state
   }
 }
