a606f6529dc81333fe6bc7b202f26cc600b2a2f3
Ma6upa <vaud55@gmail.com>
2023-04-25 19:32:56 +0400

    basic tasksPage layout

--- src/pages/tasksPage.jsx
+++ src/pages/tasksPage.jsx
@@ -1,6 +1,24 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  Card,
+  Container,
+  CssBaseline,
+  Divider,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+} from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
 import { useEffect } from "react"
+import styles from '../styles/tasksPage.module.css';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 const TasksPage = () => {
+  const theme = createTheme();
 
   useEffect(() => {
     if (!localStorage.getItem('userKey')) {
@@ -9,7 +27,51 @@ const TasksPage = () => {
   }, [])
 
   return (
-    <div>TasksPage is working</div>
+    <>
+      <AppBar position="static" className={styles.header}>
+        <Toolbar variant="dense">
+          <Button
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            <AddCircleOutlineIcon />  Новая задача
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="lg">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Card variant="outlined" className={styles.tasksWrapper}>
+              <Typography variant="h6" className={styles.tasksHeader}>
+                В очереди
+              </Typography>
+              <Divider />
+            </Card>
+            <Card variant="outlined" className={styles.tasksWrapper}>
+              <Typography variant="h6" className={styles.tasksHeader}>
+                В работе
+              </Typography>
+              <Divider />
+            </Card>
+            <Card variant="outlined" className={styles.tasksWrapper}>
+              <Typography variant="h6" className={styles.tasksHeader}>
+                Выполнено
+              </Typography>
+              <Divider />
+            </Card>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
   )
 }
 
