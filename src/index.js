d819c2ef013f1aef61364cc68f7d9922f3e14396
Ma6upa <vaud55@gmail.com>
2023-04-26 15:44:10 +0400

    fix single file bundle issues

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
-);
+window.addEventListener("DOMContentLoaded", function (e) {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  );  
+});
+
